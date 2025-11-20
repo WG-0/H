9ae26391a4da7b057a206ae327af33a273d7c086
WG-0 <3140044311@qq.com>
2025-11-20 22:42:22 +0800

    Update indexl.js

--- js/indexl.js
+++ js/indexl.js
@@ -18,7 +18,7 @@ var S = {
             m++;
       S.Shape.render();
       //console.log(m);
-      if(m==700){
+      if(m==800){
         window.location.href="../html/BirthdayCake.html";
       }
     });
